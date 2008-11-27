3126dbd5adaa8e5230006d8b084bae83fe05e367
jonmmorgan <jonmmorgan@20523bf7-2b47-0410-960a-a116b6c96ec9>
2008-11-27 11:42:38 +0000

    Removed the 0.4 tag, since it was erroneously created.
    D    https://bpbible.googlecode.com/svn/tags/release-0.4

--- tags/release-0.4/contrib/__init__.py
+++
@@ -1,7 +0,0 @@
-# ensure that any files in here don't need contrib in front of them, as the
-# contrib directory is like the site-packages directory (but with known
-# contents, and without handling for .pth files)
-import sys
-
-# TODO: insert more intelligently later on (before standard library, but after current directory, PYTHONPATH, etc.)
-sys.path.insert(0, "contrib")
