cc8a7ca326300e6e66a1cb494ad108761f88c823
jonmmorgan <jonmmorgan@20523bf7-2b47-0410-960a-a116b6c96ec9>
2009-02-28 11:46:12 +0000

    Added preferences to assist with debugging suggested on MDC.
    M    defaults/preferences/pyxpcom_gui_app.js

--- branches/xul/defaults/preferences/pyxpcom_gui_app.js
+++ branches/xul/defaults/preferences/pyxpcom_gui_app.js
@@ -1,2 +1,9 @@
 pref("general.startup.pyxpcom_gui_app",true);
 pref("toolkit.defaultChromeURI", "chrome://pyxpcom_gui_app/content/pyxpcom_gui_app.xul");
+
+/* debugging prefs */
+pref("browser.dom.window.dump.enabled", true);
+pref("javascript.options.showInConsole", true);
+pref("javascript.options.strict", true);
+pref("nglayout.debug.disable_xul_cache", true);
+pref("nglayout.debug.disable_xul_fastload", true);
