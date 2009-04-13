496fa6fbbd5d1a2d460400f26ae2aaa29f7de638
jonmmorgan <jonmmorgan@20523bf7-2b47-0410-960a-a116b6c96ec9>
2009-04-13 12:01:39 +0000

    Added basic support for using the find bar in the main window for incremental search, modelled on how Firefox does it.
    M    defaults/preferences/bpbible.js
    M    chrome/content/bpbible.xul

--- defaults/preferences/bpbible.js
+++ defaults/preferences/bpbible.js
@@ -10,3 +10,9 @@ pref("nglayout.debug.disable_xul_fastload", true);
 
 /* Extension management. */
 pref("extensions.checkCompatibility", false);
+
+/* The find bar at the bottom of the window (preferences copied from Firefox). */
+pref("accessibility.typeaheadfind", false);
+pref("accessibility.typeaheadfind.timeout", 5000);
+pref("accessibility.typeaheadfind.linksonly", false);
+pref("accessibility.typeaheadfind.flashBar", 1);
