ac24fc6b824de38b8b3b3a226e9bc7d5c2e99df0
jonmmorgan <jonmmorgan@20523bf7-2b47-0410-960a-a116b6c96ec9>
2009-04-13 12:36:40 +0000

    Converted the XUL files to proper 2-space indentation. Added a separate Javascript file to contain the Javascript functions for the main BPBible window, rather than having an inline script.
    M    chrome/content/bpbible.xul
    M    chrome/content/module_selector.xul
    AM   chrome/content/bpbible.js

---
+++ chrome/content/bpbible.js
@@ -0,0 +1,8 @@
+/**
+Function to be run when the BPBible window is loaded.
+*/
+function onLoad() {
+	window.gFindBar = document.getElementById('FindToolbar');
+}
+
+window.addEventListener("load", onLoad, false);
