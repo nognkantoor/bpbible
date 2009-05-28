0d514988761932cc98cbe8723c0182f56823e28a
benpmorgan <benpmorgan@20523bf7-2b47-0410-960a-a116b6c96ec9>
2009-05-28 07:06:54 +0000

    Highlight current verse.

--- branches/xul/chrome/content/bpbible_html.js
+++ branches/xul/chrome/content/bpbible_html.js
@@ -1,5 +1,32 @@
+function get_current_verse() {
+	var current_verse = $("a.currentverse");
+	if(current_verse.length != 1) {
+		alert("Wrong number" + current_verse.length);
+		return null;
+	}
+	return current_verse;
+}
+
+function get_current_verse_ref() {
+	var current_verse = get_current_verse();
+	return current_verse.attr("osisRef");
+}
+
+function get_current_verse_bounds() {
+	var osisRef = get_current_verse_ref();
+	var start = $('a[name="' + osisRef + '_start"]');
+	var end = $('a[name="' + osisRef + '_end"]');
+	return [start, end];
+}
+
 $(document).ready(function() {
-	var off = $(".currentverse").offset();
+	var [start, end] = get_current_verse_bounds();
+	
+	// Highlight the verse's background
+	highlight_range(start[0], end[0]);
+	
+	// Now scroll down to the right point
+	var off = start.offset();
 	var t = off.top;
 	var l = off.left;
 	/* Try and keep in middle
