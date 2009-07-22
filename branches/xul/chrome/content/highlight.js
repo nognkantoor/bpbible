f353fdecd717463ba41de1c8c19d0621bcf0b3bc
benpmorgan <benpmorgan@20523bf7-2b47-0410-960a-a116b6c96ec9>
2009-07-22 23:58:49 +0000

    Update highlight code not to highlight too much in poetry - this should work much better...

--- branches/xul/chrome/content/highlight.js
+++ branches/xul/chrome/content/highlight.js
@@ -8,7 +8,9 @@ function extract_range(r) {
 		var newNode = document.createElement("span");
 		newNode.className = "highlight";
 		r.surroundContents(newNode);
+		return true;
 	}
+	return false;
 }
 
 function highlight_range(start, end) {
@@ -23,24 +25,63 @@ function highlight_range(start, end) {
 	var s = start;
 	while (s.parentNode != common_parent) {
 		r.selectNodeContents(s.parentNode);
+		//d("Left hand side");
+		//d(s);
 		r.setStartAfter(s);
 		extract_range(r);
 		s = s.parentNode;
 	}
 	
+	/* This controls whether on our next level up, we include after this
+	 * rather than just before. If our parent is an indentedline, and we are
+	 * its last child, we want all of it to be included, otherwise the last
+	 * selected line looks wrong in poetry - it is short and on its own.
+	 * Note that we have to be careful with this - ESV Acts 15:18 starts in the
+	 * middle of a line of poetry... (thus the last child check)
+	 */
+	var try_indented = false;
 	var e = end;
 	// Now, we need to travel up from our end point, collecting everything to
 	// the left of our start node.
+	/* Note: if we are doing an indentedline, we want *all* of it */
 	while (e.parentNode != common_parent) {
 		r.selectNodeContents(e.parentNode);
+		//d("Right hand side");
+		//d(e);
 		r.setEndBefore(e);
-		extract_range(r);
+		var set_end_after = false;
+		if(e.className.match(/\bindentedline\b/))
+		{
+			// If we were in
+			r.setEndAfter(e);
+			set_end_after = true;
+		}
+			
+		var try_this_indented = try_indented;
+		if (e.parentNode.className.match(/\bindentedline\b/) 
+			&& e == e.parentNode.lastChild) 
+			try_indented = true;
+		
+		if(extract_range(r) && try_this_indented) {
+			//d("Moving up a level");
+			// We just put in another level in extract_range - we don't want
+			// to do it again - infinite loops result...
+			e = e.parentNode;
+		}
+
 		e = e.parentNode;
 	}
 
 	/* and now we need to get the part at the top of our range, between the
 	 * two topmost points */
-	r.setStartBefore(s);
-	r.setEndAfter(e);
+	//d("Both");
+	//d(s);
+	//d(e);
+	
+	r.setStartAfter(s);
+	r.setEndBefore(e);
+	if(try_indented)
+		r.setEndAfter(e);
+	
 	extract_range(r);
 }
